6794b3fc029d4633bc2e65e40d425b20cb6307ba
Martin Erpicum <martin@erpicum.net>
2024-01-03 13:06:28 +0100

    remove useless component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
-<div class="hero bg-base-100">
-  <div class="hero-content text-center flex flex-row">
-  <MainComponent :company="company_name" class=""/>
-  <ProjectsComponent class="" />
+<div class="bg-base-100">
+  <div class="flex flex-row md:flex text-center">
+  <ProjectsComponent class="p-8" />
   </div>
 </div>
   <FooterComponent :company="company_name" class="text-center flex flex-col" />
 </template>
 
 <script>
-import MainComponent from './components/MainComponent.vue'
 import FooterComponent from './components/FooterComponent.vue'
 import ProjectsComponent from './components/ProjectsComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    MainComponent,
     FooterComponent,
     ProjectsComponent
   },
--- src/components/ProjectsComponent.vue
+++ src/components/ProjectsComponent.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="bg-white py-16 sm:py-16">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl lg:mx-0">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Current projects</h2>
+        <div class="mx-auto">
+          <h1 class="text-5xl font-bold">Mesylab SRL</h1>
+          <p class="py-6">
+            Software company from Belgium managed by Martin Erpicum (<a href="https://www.linkedin.com/in/martinerpicum/" target="_blank" rel="noopener" class="link link-primary">LinkedIn</a>, <a href="https://www.erpicum.net/" target="_blank" rel="noopener" class="link link-primary">Blog</a>)
+          </p>
           <p class="mt-2 text-lg leading-8 text-gray-600">Some of my projects are listed here.</p>
         </div>
         <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
